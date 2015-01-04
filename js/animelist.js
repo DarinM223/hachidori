2547e93c2700845aab4bf9bcd8691a7238b5a81a
Darin Minamoto <darinm223@gmail.com>
2015-01-04 01:29:51 -0800

    Gets anime library through api and also has a localstorage backup

--- js/animelist.js
+++ js/animelist.js
@@ -1,6 +1,6 @@
-var HummingbirdAnimeList = (function($, HBAccessToken) {
-  if (HBAccessToken === null) {
-    alert('Error loading access token script');
+var HummingbirdAnimeList = (function($, _) {
+  if (HummingbirdAccessToken === null || _ === null) {
+    alert('Error loading script');
   }
 
   /**
@@ -11,7 +11,7 @@ var HummingbirdAnimeList = (function($, HBAccessToken) {
     this.username = username;
     this.access_token = null;
     this.anime_list = null;
-    this.getList(); // asynchronously retrive anime list
+    this.getList(function() {}); // asynchronously retrive anime list
   }
   
   /**
@@ -19,12 +19,45 @@ var HummingbirdAnimeList = (function($, HBAccessToken) {
    * @param {function(err)} callback called once finished getting the list
    */
   Animelist.prototype.getList = function getList(callback) {
-    // TODO: send ajax call to hummingbird to load the list
+    var _callback = callback;
     $.ajax({
-    }).done(function(msg) {
-      alert('Data saved: ' + msg);
+      type: 'GET',
+      url: 'http://hummingbird.me/api/v1/users/' + this.username + '/library',
+      success: function(data, textStatus, jqXHR) {
+        this.anime_list = data;
+        if (typeof(Storage) !== 'undefined') { // save anime list into local storage
+          localStorage.setItem(this.username, JSON.stringify(this.anime_list)); // TODO: call this in webworker
+        }
+        _callback(null);
+      }.bind(this),
+      error: function(jqXHR, textStatus, error) {
+        if (typeof(Storage) !== 'undefined') { // load anime list from local storage
+          this.anime_list = JSON.parse(localStorage.getItem(this.username)); // TODO: call this in webworker
+        }
+        _callback(error);
+      }.bind(this)
     });
   };
 
+  Animelist.prototype.getCurrentlyWatching = function getCurrentlyWatching() {
+    return _.where(this.anime_list, { status: 'currently-watching' });
+  };
+
+  Animelist.prototype.getCompleted = function getCompleted() {
+    return _.where(this.anime_list, { status: 'completed' });
+  };
+
+  Animelist.prototype.getPlanToWatch = function getPlanToWatch() {
+    return _.where(this.anime_list, { status: 'plan-to-watch' });
+  };
+
+  Animelist.prototype.getOnHold = function getOnHold() {
+    return _.where(this.anime_list, { status: 'on-hold' });
+  };
+
+  Animelist.prototype.getDropped = function getDropped() {
+    return _.where(this.anime_list, { status: 'dropped' });
+  };
+
   return Animelist;
-}) (jQuery, HummingbirdAccessToken);
+}) (jQuery, _.noConflict());
