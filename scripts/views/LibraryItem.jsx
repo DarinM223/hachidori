5aab8cd4da2553f2f0847a121925448d8f8524b5
Darin Minamoto <darinm223@gmail.com>
2015-01-10 18:29:42 -0800

    Fixed indent problem, implemented incrementing episodes and automatic button disable after completed

--- scripts/views/LibraryItem.jsx
+++ scripts/views/LibraryItem.jsx
@@ -2,7 +2,7 @@
  * @property {LibraryItem} libraryItem
  */
 var LibraryItemComponent = React.createClass({
-  getInitalState: function() {
+  getInitialState: function() {
     var retValue = {};
     if (this.props.libraryItem.anime.title === null) {
       retValue.title = "";
@@ -10,29 +10,58 @@ var LibraryItemComponent = React.createClass({
       retValue.title = this.props.libraryItem.anime.title;
     }
 
-    if (this.props.libraryItem.episodes_watched === null) {
-      retValue.episodes_watched = 0;
-    } else {
-      retValue.episodes_watched = this.props.libraryItem.episodes_watched;
-    }
+    retValue.episodes_watched = this.props.libraryItem.episodes_watched;
+    retValue.total_episodes = this.props.libraryItem.anime.episode_count;
     return retValue;
   },
   onIncrementClicked: function() {
-    this.setState({
-      episodes_watched: this.state.episodes_watched+1
-    });
+    if (this.state.episodes_watched !== null && 
+        (this.state.episodes_watched < this.state.total_episodes || this.state.total_episodes === null)) {
+      this.setState({
+        episodes_watched: this.state.episodes_watched+1
+      });
+    } 
   },
   titleStyle: {
-    display: 'inline',
-    verticalAlign: 'middle'
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    overflow: 'hidden'
+  },
+  episodeStyle: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    float: 'right',
+    clear: 'right',
+    overflow: 'hidden'
+  },
+  spacerStyle: {
+    width: '0px',
+    float: 'right'
   },
   render: function() {
+    var episodesWatchedText = this.state.episodes_watched;
+    var totalEpisodesText = this.state.total_episodes;
+    if (this.state.episodes_watched === null) {
+      episodesWatchedText = '_';
+    }
+    if (this.state.total_episodes === null) {
+      totalEpisodesText = '_';
+    }
+
+    var button = <input type="button" className="btn btn-warning btn-lg disabled" value="^"/>
+    if (this.state.total_episodes === null || 
+                     this.state.episodes_watched < this.state.total_episodes) {
+      button = <input type="button" className="btn btn-warning btn-lg" onClick={this.onIncrementClicked} value="^"/>
+    } 
+
     return (
       <li className="list-group-item">
-        <input type="button" className="btn btn-warning btn-lg" onClick={this.onIncrementClicked} value="^"/>
+        {button}
         &nbsp;
         &nbsp;
-      	<h1 style={this.titleStyle}>{this.props.libraryItem.anime.title}</h1>
+        <div style={this.spacerStyle} className="spacer"></div>
+        <h1 style={this.episodeStyle}>{episodesWatchedText}/{totalEpisodesText}</h1>
+      	<h1 style={this.titleStyle}>{this.state.title}</h1>
       </li>
     );
   }
