666b48868940d4a9243701d74ebf5e49d125238e
Darin Minamoto <darinm223@gmail.com>
2015-06-24 22:20:30 -0700

    Attempted fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 var PORT = process.env.PORT;
 
